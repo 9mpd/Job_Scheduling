b2b9c7e2967a94c952c8bed963c8abfaa13a33de
Narayan Jee Jha <58243776+9mpd@users.noreply.github.com>
2021-06-09 12:26:18 +0530

    Update Run Time Comparison Graph.m
    License Added.

--- Run Time Comparison Graph.m
+++ Run Time Comparison Graph.m
@@ -1,3 +1,9 @@
+Copyright (c) 2021, 9mpd
+All rights reserved.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+
 % Plotting the Experimental Results of Greedy Algorithm &
 % Brute Force Algorithm in Job Scheduling.
 
